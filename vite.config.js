a0d6b3b0a1b192dff9697f97ae1db84654e73470
kasra paketeghad <kasrapak69@gmail.com>
2022-09-13 10:59:33 +0430

    Installed styled-components

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Sets firefox as default browser.
 process.env.BROWSER = 'firefox';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Starting server will open the browser automatically.
   server: {
     open: '/',
   },
