bcb8e463856b88425b1434d5a2aca93575163f7f
kasra paketeghad <kasrapak69@gmail.com>
2022-09-13 18:52:34 +0430

    Added SVGR

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import * as path from 'path';
 
 // Sets firefox as default browser.
@@ -7,7 +8,7 @@ process.env.BROWSER = 'firefox';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react()],
   // Starting server will open the browser automatically.
   server: {
     open: '/',
