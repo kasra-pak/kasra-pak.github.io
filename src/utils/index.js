b062878bf29d903936749eedce9a59138cac1599
kasra paketeghad <kasrapak69@gmail.com>
2022-09-28 08:46:30 +0330

    Added Utils: Debounce Function

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+function debounce(func, ms) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, arguments), ms);
+  };
+}
+
+export { debounce };
