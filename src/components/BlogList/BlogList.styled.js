785b0b16a88f48caced3047135494c4cb7e67cbc
kasra paketeghad <kasrapak69@gmail.com>
2023-03-06 16:00:36 +0330

    Enable multi column blog list on big screens

--- src/components/BlogList/BlogList.styled.js
+++ src/components/BlogList/BlogList.styled.js
@@ -6,17 +6,12 @@ const Wrapper = styled.div`
   gap: 1em;
   margin-block-start: 2em;
 
-  & > article {
-    max-width: 520px;
-  }
-
-  @media screen and (min-width: 1223px) {
+  @media screen and (min-width: 1200px) {
     flex-direction: row;
     flex-wrap: wrap;
 
-    & > article {
-      min-width: 400px;
-      flex: 0 0 40%;
+    & > a {
+      flex: 0 1 500px;
     }
   }
 `;
