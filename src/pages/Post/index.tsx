ad3a1c930e786d9624116db8fbaa55930f1d4789
kasra paketeghad <kasrapak69@gmail.com>
2023-03-09 10:52:11 +0330

    Solve type errors Post page

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import data from '@/data/blogPosts.json';
@@ -14,20 +13,20 @@ const Post = () => {
     <S.Wrapper>
       <article>
         <S.Flex>
-          <S.Date>{post.date}</S.Date>
+          <S.Date>{post?.date}</S.Date>
           <S.Divider />
-          <S.Duration>{post.duration} min read</S.Duration>
+          <S.Duration>{post?.duration} min read</S.Duration>
           <S.ShareBtn>
             <ShareSVG />
           </S.ShareBtn>
         </S.Flex>
-        <S.Title>{post.title}</S.Title>
+        <S.Title>{post?.title}</S.Title>
         <S.Topics>
-          {post.topics.map((topic, idx) => (
+          {post?.topics.map((topic, idx) => (
             <li key={idx}>{topic}</li>
           ))}
         </S.Topics>
-        <S.PostContent>{post.content}</S.PostContent>
+        <S.PostContent>{post?.content}</S.PostContent>
       </article>
       <section style={{ border: '2px solid black', flex: '1 0 30%' }}>
         <h2>Similar Posts</h2>
